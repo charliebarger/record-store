207683f3e1f14f580306206646950a515f8be69f
charliebarger <charliebarger04@gmail.com>
2022-01-10 09:47:02 -0700

    change url

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/new-genre", newCategoryRouter);
-app.use("/new-item", newItemRouter);
+app.use("/new-record", newItemRouter);
 app.use("/item", detailRouter);
 
 // catch 404 and forward to error handler
